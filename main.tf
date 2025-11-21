69630228fe3723dc87b1ccff7bfa2add4b992f6e
kushal <dkushal.prasad@harness.io>
2025-11-21 15:34:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
 terraform {
   required_version = ">= 1.0"
-  
-  cloud {
-    organization = "test-harness-logs"
-    
-    workspaces {
-      name = "harness-test-workspace"
-    }
-  }
 }
 
 # Simple output that doesn't require any resources
